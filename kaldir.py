c5836dd41967cb092c02e6301b7e4da60f3863ad
Omer Sahin <bilgi@omersahin.com.tr>
2025-01-13 14:20:14 +0300

    ilk commit atildi

---
+++ kaldir.py
@@ -0,0 +1,11 @@
+from rembg import remove
+
+input_path = "kedi.jpg"
+output_path = "cat.jpg"
+
+with open(input_path,'rb') as i: ##dosyayı rb-read binary olarak açar
+    with open(output_path,'wb') as o: ## dosyayı yazar binary olarak
+        in_file = i.read()
+        output_file = remove(in_file)
+        o.write(output_file)
+
